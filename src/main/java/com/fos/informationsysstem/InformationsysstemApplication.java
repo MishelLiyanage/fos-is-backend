50f1374538e79e0d34284d87fb9bae214ca07cae
MishelLiyanage <mailtomishel@gmail.com>
2024-08-12 19:43:13 +0530

    adding initial files

---
+++ src/main/java/com/fos/informationsysstem/InformationsysstemApplication.java
@@ -0,0 +1,13 @@
+package com.fos.informationsysstem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationsysstemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationsysstemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fos/informationsysstem/InformationsysstemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fos.informationsysstem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationsysstemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
